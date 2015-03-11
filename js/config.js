a6344bdd0907781856659ca1564b420bb8008ce8
thatknitter <kblewis.flt@gmail.com>
2015-03-11 09:36:06 -0500

    config finished

---
+++ js/config.js
@@ -0,0 +1,25 @@
+;(function(){
+  'use strict';
+  
+  angular.module('portApp')
+    .config(function($routeProvider){
+      $routeProvider 
+        .when('/', {
+          templateUrl: 'views/home.html'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html'
+        })
+        .when('/work', {
+          templateUrl: 'views/work.html'
+        })
+        .when('/resume', {
+          templateUrl: 'views/resume.html'
+        })
+        .when('/contact', {
+          templateUrl: 'views/contact.html'
+        })
+        .otherwise({redirectTo: '/'});
+    });
+  
+});
