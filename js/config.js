3e24763538ba9c89c57b44096618eaa6cbcb09de
thatknitter <kblewis.flt@gmail.com>
2015-03-13 13:53:06 -0500

    Repared problems, beginning css

--- js/config.js
+++ js/config.js
@@ -3,23 +3,23 @@
   
   angular.module('portApp')
     .config(function($routeProvider){
-      $routeProvider 
-        .when('/', {
-          templateUrl: 'views/home.html'
-        })
-        .when('/about', {
-          templateUrl: 'views/about.html'
-        })
-        .when('/work', {
-          templateUrl: 'views/work.html'
-        })
-        .when('/resume', {
-          templateUrl: 'views/resume.html'
-        })
-        .when('/contact', {
-          templateUrl: 'views/contact.html'
-        })
-        .otherwise({redirectTo: '/'});
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/work', {
+        templateUrl: 'views/work.html'
+      })
+      .when('/resume', {
+        templateUrl: 'views/resume.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .otherwise({redirectTo: '/'});
     });
   
-});
+}());
